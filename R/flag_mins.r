1dfad240c46f8d0d87d63e21d6d714245d1420fb
Matt <matthewroumaya@gmail.com>
2021-01-26 16:59:22 -0500

    add flag mins function

---
+++ R/flag_mins.r
@@ -0,0 +1,32 @@
+#' @title flag_mins
+#'
+#' @description
+#' Flag survey duration based on how many minutes a respondent took. The assumption is that your data.frame contains two columns: "date_created" and "date_modified".
+#'
+#' @param dat a data.frame containing SurveyMonkey response data, with two columns "date_created" and "date_modified".
+#' @param mins a whole number specifying the cutoff for the .
+#'
+#'
+#' @return a data.frame object.
+#'
+#' @export
+flag_mins <- function(dat, mins){
+  
+  dat <- dat %>% 
+    mutate(flag_mins = round(difftime(dat$date_modified, dat$date_created, units = 'mins'),0),
+           flag_mins = ifelse(flag_mins < mins, "FLAG", NA))
+  
+  if (is.numeric(mins) == FALSE){
+    dat <- dat
+    ui_warn('"mins" accepts numeric values only; All NA returned. Try using a non-negative integer.')
+  }
+  
+  if (mins < 0) {
+    dat <- dat
+    ui_warn("Time travel does not yet exist (or does it?). Provide a non-negative number.")
+  }
+  
+  return(dat)
+  
+}
+
